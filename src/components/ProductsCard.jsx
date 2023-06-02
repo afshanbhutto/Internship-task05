5675b1e5dbb41abca9e9e14bb129b4b67a5a25e7
Afshan Bhutto <afshanbhutto6@gmail.com>
2023-06-02 22:18:25 +0500

    first commit

---
+++ src/components/ProductsCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../redux/action';
+
+
+const ProductsCard = (props) => {
+
+    const { img,  title, price } = props;
+
+    const dispatch = useDispatch();
+
+    return (
+        <>
+            <div className="product_card">
+                <figure>
+                    <img src={img} alt="item-img" />
+                </figure>
+                <h4 className="title">{title}</h4>
+                <h3 className="price">$ {price.toLocaleString()}</h3>
+                <button
+                    type="button"
+                    className="btn"
+                    onClick={() => dispatch(addToCart({ ...props }))}
+                >
+                    Add to cart
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default ProductsCard;
